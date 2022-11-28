725a1f666c332e1ff675f5ba16523358c1e40ad3
NicolasQueiroga <n.macielqueiroga@gmail.com>
2022-11-28 00:57:58 -0300

    upload

--- theme.config.js
+++ theme.config.js
@@ -1,12 +1,12 @@
 export default {
-  github: 'https://github.com/shuding/nextra',
-  docsRepositoryBase: 'https://github.com/shuding/nextra/blob/master',
-  titleSuffix: ' – Nextra',
+  github: 'https://github.com/NicolasQueiroga/my-dashboard',
+  docsRepositoryBase: 'https://github.com/NicolasQueiroga/cloud-docs',
+  titleSuffix: '',
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
+      <span className="mr-2 font-extrabold hidden md:inline">cloud-doc</span>
       <span className="text-gray-600 font-normal hidden md:inline">
-        The Next.js Static Site Generator
+        documentando o projeto de computação em nuvem
       </span>
     </>
   ),
